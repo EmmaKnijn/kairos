9eb4233809c26f803f159826fbc023e100c9760b
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2025-10-01 13:40:10 +0200

    clean up code

--- rendering/index.js
+++ rendering/index.js
@@ -7,12 +7,10 @@ const utils = require('./utils')
 
 const endpoint = process.env.ENDPOINT || 'http://192.168.130.121:8080'
 const port = process.env.PORT || 3000
+
+
 app.get('/dotmatrix/:station/:location', (req, res) => {
-    const options = {
-        path: endpoint + '/v2/departures/station/' + req.params.station.toUpperCase(),
-    };
-    res.setHeaderValue
-    utils.getJSON(options, (data) => {
+    utils.getJSON(endpoint + '/v2/departures/station/' + req.params.station.toUpperCase(), (data) => {
         for (const [key, departure] of Object.entries(data.departures)) {
             if (departure.platform_actual === req.params.location) {
                 console.log(departure)
@@ -21,8 +19,6 @@ app.get('/dotmatrix/:station/:location', (req, res) => {
             }
         }
     });
-
-    //res.send('ERR')
 })
 
 app.listen(port, () => {
--- rendering/utils.js
+++ rendering/utils.js
@@ -1,25 +1,22 @@
-const http = require('http');
-const https = require('https');
+    const http = require('http');
 
-// source: https://stackoverflow.com/questions/9577611/how-to-make-an-http-get-request-in-node-js-express
-/**
- * getJSON:  RESTful GET request returning JSON object(s)
- * @param options: http options object
- * @param callback: callback to pass the results JSON object(s) back
- */
+    // source: https://stackoverflow.com/questions/9577611/how-to-make-an-http-get-request-in-node-js-express
 
-module.exports.getJSON = (options, onResult) => {
-  console.log(options.path)
+    /**
+     * getJSON:  RESTful GET request returning JSON object(s)
+     * @param path: the endpoint to call
+     * @param callback: callback to pass the results JSON object(s) back
+     */
 
-  http.get(options.path, resp => {
-        let data = ''
-        resp.on('data', chunk => {
-            data += chunk
+    module.exports.getJSON = (path, onResult) => {
+      http.get(path, resp => {
+            let data = ''
+            resp.on('data', chunk => {
+                data += chunk
+            })
+            resp.on('end', () => {
+                data = JSON.parse(data)
+                onResult(data)
+            })
         })
-        resp.on('end', () => {
-            data = JSON.parse(data)
-            onResult(data)
-        })
-    })
-    return {}
-};
+    };
